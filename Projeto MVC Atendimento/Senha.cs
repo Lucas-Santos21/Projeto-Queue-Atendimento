f516f7665a00158329e390fcdb2e66960fc904be
Lucas Santos <lucassantos135b211@gmail.com>
2025-10-27 20:07:29 -0300

    classe Senha finalizada

--- Projeto MVC Atendimento/Senha.cs
+++ Projeto MVC Atendimento/Senha.cs
@@ -15,11 +15,61 @@ namespace Projeto_MVC_Atendimento
         private DateTime? _dataAtend;
         private DateTime? _horaAtend;
 
+        //Construtor
+        public Senha(int id)
+        {
+            Id = id;
+            DataGerac = DateTime.Now.Date;
+            HoraGerac = DateTime.Now;
+            DataAtend = null;
+            HoraAtend = null;
+        }
+
         //propriedades privadas
         public int Id
         {
             get { return _id; }
             set { _id = value; }
         }
+
+        public DateTime DataGerac
+        {
+            get { return _dataGerac; }
+            set { _dataGerac = value; }
+        }
+
+        public DateTime HoraGerac
+        {
+            get { return _horaGerac; }
+            set { _horaGerac = value; }
+        }
+
+        public DateTime? DataAtend
+        {
+            get { return _dataAtend; }
+            set { _dataAtend = value; }
+        }
+
+        public DateTime? HoraAtend
+        {
+            get { return _horaAtend; }
+            set { _horaAtend = value; }
+        }
+
+        //Metodos Publicos
+        public string dadosParciais()
+        {
+            string saida = $"{Id}-{DataGerac:dd/MM/yyyy}-{HoraGerac:HH:mm}";
+
+            return saida;
+        }
+
+        public string dadosCompletos()
+        {
+            string saida = $"{Id}-{DataGerac:dd/MM/yyyy}-{HoraGerac:HH:mm}-{DataAtend:dd/MM/yyyy}-{HoraAtend:HH:mm}";
+
+            return saida;
+        }
+
     }
 }
