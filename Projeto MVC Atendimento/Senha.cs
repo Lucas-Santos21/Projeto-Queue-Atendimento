f31e992810e5185018803ddc79e606efb906ce39
Lucas Santos <lucassantos135b211@gmail.com>
2025-10-27 18:32:59 -0300

    primeiro commit

---
+++ Projeto MVC Atendimento/Senha.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_MVC_Atendimento
+{
+    class Senha
+    {
+        //Propriedades
+        private int _id;
+        private DateTime _dataGerac;
+        private DateTime _horaGerac;
+        private DateTime? _dataAtend;
+        private DateTime? _horaAtend;
+
+        //propriedades privadas
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+    }
+}
