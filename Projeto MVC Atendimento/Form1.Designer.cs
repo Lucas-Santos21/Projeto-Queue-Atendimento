728290143c3716efd7a891b7de5b54e20b9f464a
Lucas Santos <lucassantos135b211@gmail.com>
2025-11-10 16:46:42 -0300

    update em program

--- Projeto MVC Atendimento/Form1.Designer.cs
+++ Projeto MVC Atendimento/Form1.Designer.cs
@@ -28,13 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.btnListarSenhas = new System.Windows.Forms.Button();
+            this.btnAdicionarGuiche = new System.Windows.Forms.Button();
+            this.btnChamar = new System.Windows.Forms.Button();
+            this.btnListarAtendimentos = new System.Windows.Forms.Button();
+            this.lstSenhas = new System.Windows.Forms.ListBox();
+            this.lstAtendimentos = new System.Windows.Forms.ListBox();
+            this.lblQtdGuiches = new System.Windows.Forms.Label();
+            this.txtGuiche = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(119, 81);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(95, 23);
+            this.btnGerar.TabIndex = 0;
+            this.btnGerar.Text = "Gerar";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // btnListarSenhas
+            // 
+            this.btnListarSenhas.Location = new System.Drawing.Point(119, 304);
+            this.btnListarSenhas.Name = "btnListarSenhas";
+            this.btnListarSenhas.Size = new System.Drawing.Size(95, 28);
+            this.btnListarSenhas.TabIndex = 1;
+            this.btnListarSenhas.Text = "Listar Senhas";
+            this.btnListarSenhas.UseVisualStyleBackColor = true;
+            this.btnListarSenhas.Click += new System.EventHandler(this.btnListarSenhas_Click);
+            // 
+            // btnAdicionarGuiche
+            // 
+            this.btnAdicionarGuiche.Location = new System.Drawing.Point(288, 199);
+            this.btnAdicionarGuiche.Name = "btnAdicionarGuiche";
+            this.btnAdicionarGuiche.Size = new System.Drawing.Size(85, 38);
+            this.btnAdicionarGuiche.TabIndex = 2;
+            this.btnAdicionarGuiche.Text = "Adicionar";
+            this.btnAdicionarGuiche.UseVisualStyleBackColor = true;
+            this.btnAdicionarGuiche.Click += new System.EventHandler(this.btnAdicionarGuiche_Click);
+            // 
+            // btnChamar
+            // 
+            this.btnChamar.Location = new System.Drawing.Point(583, 78);
+            this.btnChamar.Name = "btnChamar";
+            this.btnChamar.Size = new System.Drawing.Size(75, 23);
+            this.btnChamar.TabIndex = 3;
+            this.btnChamar.Text = "Chamar";
+            this.btnChamar.UseVisualStyleBackColor = true;
+            this.btnChamar.Click += new System.EventHandler(this.btnChamar_Click);
+            // 
+            // btnListarAtendimentos
+            // 
+            this.btnListarAtendimentos.Location = new System.Drawing.Point(431, 309);
+            this.btnListarAtendimentos.Name = "btnListarAtendimentos";
+            this.btnListarAtendimentos.Size = new System.Drawing.Size(200, 23);
+            this.btnListarAtendimentos.TabIndex = 4;
+            this.btnListarAtendimentos.Text = "Listar Atendimentos";
+            this.btnListarAtendimentos.UseVisualStyleBackColor = true;
+            this.btnListarAtendimentos.Click += new System.EventHandler(this.btnListarAtendimentos_Click);
+            // 
+            // lstSenhas
+            // 
+            this.lstSenhas.FormattingEnabled = true;
+            this.lstSenhas.Location = new System.Drawing.Point(77, 110);
+            this.lstSenhas.Name = "lstSenhas";
+            this.lstSenhas.Size = new System.Drawing.Size(189, 173);
+            this.lstSenhas.TabIndex = 5;
+            // 
+            // lstAtendimentos
+            // 
+            this.lstAtendimentos.FormattingEnabled = true;
+            this.lstAtendimentos.Location = new System.Drawing.Point(397, 110);
+            this.lstAtendimentos.Name = "lstAtendimentos";
+            this.lstAtendimentos.Size = new System.Drawing.Size(276, 173);
+            this.lstAtendimentos.TabIndex = 6;
+            // 
+            // lblQtdGuiches
+            // 
+            this.lblQtdGuiches.AutoSize = true;
+            this.lblQtdGuiches.Location = new System.Drawing.Point(322, 172);
+            this.lblQtdGuiches.Name = "lblQtdGuiches";
+            this.lblQtdGuiches.Size = new System.Drawing.Size(13, 13);
+            this.lblQtdGuiches.TabIndex = 7;
+            this.lblQtdGuiches.Text = "0";
+            // 
+            // txtGuiche
+            // 
+            this.txtGuiche.Location = new System.Drawing.Point(477, 81);
+            this.txtGuiche.Name = "txtGuiche";
+            this.txtGuiche.Size = new System.Drawing.Size(100, 20);
+            this.txtGuiche.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(289, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Qtde de Senhas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(413, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Guiche:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(850, 476);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGuiche);
+            this.Controls.Add(this.lblQtdGuiches);
+            this.Controls.Add(this.lstAtendimentos);
+            this.Controls.Add(this.lstSenhas);
+            this.Controls.Add(this.btnListarAtendimentos);
+            this.Controls.Add(this.btnChamar);
+            this.Controls.Add(this.btnAdicionarGuiche);
+            this.Controls.Add(this.btnListarSenhas);
+            this.Controls.Add(this.btnGerar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.Button btnListarSenhas;
+        private System.Windows.Forms.Button btnAdicionarGuiche;
+        private System.Windows.Forms.Button btnChamar;
+        private System.Windows.Forms.Button btnListarAtendimentos;
+        private System.Windows.Forms.ListBox lstSenhas;
+        private System.Windows.Forms.ListBox lstAtendimentos;
+        private System.Windows.Forms.Label lblQtdGuiches;
+        private System.Windows.Forms.TextBox txtGuiche;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Projeto MVC Atendimento/Form1.cs
+++ Projeto MVC Atendimento/Form1.cs
@@ -12,9 +12,84 @@ namespace Projeto_MVC_Atendimento
 {
     public partial class Form1 : Form
     {
+        Senhas senhas = new Senhas();
+        Guiches guiches = new Guiches();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            senhas.gerar();
+            MessageBox.Show("Senha gerada com sucesso!");
+        }
+
+        private void btnListarSenhas_Click(object sender, EventArgs e)
+        {
+            lstSenhas.Items.Clear();
+
+            foreach (Senha s in senhas.FilaSenhas)
+            {
+                lstSenhas.Items.Add(s.dadosParciais());
+            }
+        }
+
+        private void btnAdicionarGuiche_Click(object sender, EventArgs e)
+        {
+            Guiche novo = new Guiche(guiches.ListaGuiches.Count + 1);
+            guiches.Adicionar(novo);
+
+            lblQtdGuiches.Text = guiches.ListaGuiches.Count.ToString();
+            MessageBox.Show("Guichê adicionado com sucesso!");
+        }
+
+        private void btnChamar_Click(object sender, EventArgs e)
+        {
+            int numeroGuiche;
+
+            if (!int.TryParse(txtGuiche.Text, out numeroGuiche))
+            {
+                MessageBox.Show("Informe um número de guichê válido!");
+                return;
+            }
+
+            if (numeroGuiche <= 0 || numeroGuiche > guiches.ListaGuiches.Count)
+            {
+                MessageBox.Show("Guichê inexistente!");
+                return;
+            }
+
+            Guiche g = guiches.ListaGuiches[numeroGuiche - 1];
+
+            bool chamado = g.Chamar(senhas.FilaSenhas);
+
+            if (chamado)
+            {
+                Senha ultima = g.Atendimentos.Peek();
+                ultima.DataAtend = DateTime.Now.Date;
+                ultima.HoraAtend = DateTime.Now;
+
+                MessageBox.Show($"Senha {ultima.Id} chamada no guichê {g.Id}.");
+            }
+            else
+            {
+                MessageBox.Show("Não há senhas na fila!");
+            }
+        }
+
+        private void btnListarAtendimentos_Click(object sender, EventArgs e)
+        {
+            lstAtendimentos.Items.Clear();
+
+            foreach (Guiche g in guiches.ListaGuiches)
+            {
+                foreach (Senha s in g.Atendimentos)
+                {
+                    lstAtendimentos.Items.Add(s.dadosCompletos());
+                }
+            }
         }
     }
 }
